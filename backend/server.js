42534bf269850521c431f3ad1b331277a538b11f
charith02 <94479302+charith02@users.noreply.github.com>
2024-10-06 23:51:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://designersworkroom-in-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json()); // Parse JSON request bodies
 
 // Database connection
