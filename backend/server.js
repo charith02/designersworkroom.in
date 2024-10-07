5f077f3145cb4d3c330b53f6e9015c45c24b7e0c
charith02 <charithranguri@gmail.com>
2024-10-07 21:31:44 +0530

    commit16

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.error("MongoDB connection error:", err));
 
-app.length("/", (req, res) => {
+app.get("/", (req, res) => {
     res.json("Hello");
 })
 
