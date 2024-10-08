2d93eee729b4b5bb360c4a323217583e88045fb9
charith02 <charithranguri@gmail.com>
2024-10-08 14:36:11 +0530

    commit31

--- api/controllers/queryController.js
+++ api/controllers/queryController.js
@@ -1,18 +1,21 @@
 // controllers/queryController.js
-const query = require('../models/query');
+const Query = require('../models/Query');
 
 // Function to handle query submission
 exports.submitQuery = async (req, res) => {
     const { name, email, subject, message } = req.body;
-  
+
     try {
-        console.log("Form Data:", { name, email, subject, message });
-        const newQuery = new query({ name, email, subject, message });
-        await newQuery.save(); 
+        const newQuery = new Query({
+            name,
+            email,
+            subject,
+            message
+        });
+
+        await newQuery.save(); // Save the query to MongoDB
         res.status(201).json({ message: "Query submitted successfully!" });
     } catch (error) {
-        console.error("Error saving query:", error);
         res.status(500).json({ error: "Failed to submit query" });
     }
-  };
-  
+};
