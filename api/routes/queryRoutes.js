288c5ec8c89d891c0afc68c273045c994ebae8f8
charith02 <charithranguri@gmail.com>
2024-10-10 15:24:33 +0530

    commit47

--- api/routes/queryRoutes.js
+++ api/routes/queryRoutes.js
@@ -4,9 +4,6 @@ const router = express.Router();
 const { submitQuery } = require('../controllers/queryController');
 
 // Route to handle query form submission
-router.post('/submit-query', (req, res) => {
-    console.log('Submit query route hit');
-    res.json({ message: "Test route hit" });
-  });
+router.post('/submit-query', submitQuery);
 
 module.exports = router;
