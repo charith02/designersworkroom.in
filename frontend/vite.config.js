52d4ccb498cbb1c7dce7cbd0af24b691c52cb831
charith02 <charithranguri@gmail.com>
2024-10-25 19:36:37 +0530

    commit 58

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/designersworkroom.in/', // Update this to match your repo name
   build: {
     rollupOptions: {
-      external: ['@fortawesome/react-fontawesome', '@fortawesome/free-brands-svg-icons'],
+      external: [
+        '@fortawesome/react-fontawesome', 
+        '@fortawesome/free-brands-svg-icons'
+      ],
     },
   },
 });
