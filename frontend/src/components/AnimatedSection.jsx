1b1bceb49b25c6bd3befa822169c5b1f22f2a70e
charith02 <94479302+charith02@users.noreply.github.com>
2025-01-08 14:47:50 +0530

    Update AnimatedSection.jsx

--- frontend/src/components/AnimatedSection.jsx
+++ frontend/src/components/AnimatedSection.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './AnimatedSection.css';
+import GTM from './GTM.js';
 import dataLow from '../data/dataLow';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
